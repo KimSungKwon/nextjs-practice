f6a3efe75e15f905210daa0cf072c9953870db3b
KimSungKwon <sungkwonn@naver.com>
2023-03-26 16:05:43 +0900

    ChakraUI 예

---
+++ 07useChakraUI/pages/_app.js
@@ -0,0 +1,13 @@
+import { ChakraProvider, Box } from '@chakra-ui/react';
+import TopBar from '../components/TopBar';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <ChakraProvider>
+      <TopBar />
+      <Box maxWidth="container.xl" margin="auto">
+        <Component {...pageProps} />
+      </Box>
+    </ChakraProvider>
+  )
+}
---
+++ 07useChakraUI/pages/_document.js
@@ -0,0 +1,21 @@
+import { ColorModeScript, extendTheme } from '@chakra-ui/react';
+import { Html, Head, Main, NextScript } from 'next/document'
+
+const config = {
+  useSystemColorMode: true,
+};
+
+const theme = extendTheme({ config });
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
