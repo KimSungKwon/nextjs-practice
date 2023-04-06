3df75f4422c4474fb468eb65fcaefd4a961cc40e
KimSungKwon <sungkwonn@naver.com>
2023-04-06 19:37:30 +0900

    dummy data

---
+++ 09_Test/boilerplate/components/ArticleCard/tests/mock.js
@@ -0,0 +1,13 @@
+export const article = {
+    id: 'u12w3o0d',
+    title: 'Healthy summer melon-carrot soup',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipisicing ellit morbi iaculis, felis quis sagittis molestie, mi sem lobortis',
+    author: {
+        id: '93ksj19s',
+        name: 'John Doe',
+    },
+    image: {
+        url: 'https://images.unsplash.com/photo-1629032355262-d751086c475d',
+        author: 'Karolin Baitinger',
+    },
+};
