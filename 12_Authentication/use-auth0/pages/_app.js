21a0887fc6136c618061fa14fd588d2cdc1058f6
KimSungKwon <sungkwonn@naver.com>
2023-04-12 22:49:49 +0900

    set auth0

--- 12_Authentication/use-auth0/pages/_app.js
+++ 12_Authentication/use-auth0/pages/_app.js
@@ -1,5 +1,10 @@
-
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    // 어플리케이션에서 페이지 간 세션이 계속 유지되도록
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
