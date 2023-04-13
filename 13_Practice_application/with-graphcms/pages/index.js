d2507e127b3b82a8ebda4ebdb9c52ffa31f698aa
KimSungKwon <sungkwonn@naver.com>
2023-04-13 21:57:27 +0900

    index page

--- 13_Practice_application/with-graphcms/pages/index.js
+++ 13_Practice_application/with-graphcms/pages/index.js
@@ -1,8 +1,25 @@
+import { Grid } from '@chakra-ui/layout';
+import ProductCard from "@/components/ProductCard";
+import graphql from "@/lib/graphql";
+import getAllProducts from "@/lib/graphql/queries/getAllProducts";
 
-export default function Home() {
+export const getStaticProps = async () => {
+  const { products } = await graphql.request(getAllProducts)
+
+  return {
+    props: {
+      revalidate: 60, // 증분 정적 재생성 방식. 60초마다
+      products,
+    },
+  }
+}
+
+export default function Home(props) {
   return (
-    <>
-      this is home
-    </>
+    <Grid gridTemplateColumns="repeat(4, 1fr)" gap="5">
+      {props.products.map((product) => (
+        <ProductCard key={product.id} {...product} />
+      ))}
+    </Grid>
   )
 }
