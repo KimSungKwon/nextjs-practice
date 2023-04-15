b133935f929f28c2f8f6d01749fd0e410405f04c
KimSungKwon <sungkwonn@naver.com>
2023-04-15 17:07:52 +0900

    create Navigation Bar

--- 13_Practice_application/with-graphcms/pages/_app.js
+++ 13_Practice_application/with-graphcms/pages/_app.js
@@ -1,13 +1,14 @@
 import { Box, Flex, ChakraProvider } from "@chakra-ui/react"
-
+import NavBar from "@/components/NavBar"
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <Flex w="full" minH="100vh" bgColor="gray.100">
-          <Box maxW="70vw" m="auto">
-            <Component {...pageProps} />
-          </Box>
+        <NavBar />
+        <Box maxW="70vw" m="auto">
+          <Component {...pageProps} />
+        </Box>
       </Flex>
     </ChakraProvider>
   )
